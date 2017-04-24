5dec3ff415083b9a9572f1f8c7ef4c649d16169d
darkobits <joshua@frontlawn.net>
2017-04-23 20:59:52 -0700

    Unity: digest() flushes pending $applyAsync calls.

--- packages/unity/src/utils/digest.js
+++ packages/unity/src/utils/digest.js
@@ -4,7 +4,7 @@ import {
 
 
 /**
- * Run a digest cycle on the root scope.
+ * Run a digest cycle on the root scope and flush any pending $applyAsync calls.
  *
  * @example
  *
@@ -16,4 +16,16 @@ import {
  */
 export function digest () {
   get('$rootScope').$digest();
+
+  // This will ensure any pending $applyAsync calls are run.
+  // See: https://github.com/angular/angular.js/issues/10788
+  try {
+    get('$browser').defer.flush();
+  } catch (err) {
+    // If there were no pending async tasks to flush, ignore the error.
+    // Otherwise, re-throw it.
+    if (!/No deferred tasks to be flushed/g.test(err.message)) {
+      throw err;
+    }
+  }
 }
